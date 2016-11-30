1743d3ea4acff9d67b10849cf984898c1ec65b10
jasontrpanda <jasonacg@gmail.com>
2016-12-01 03:16:37 +0900

    upload NCR to UTF8 Tool

---
+++ NCR2utf8/NCR2utf8.bat
@@ -0,0 +1,3 @@
+@echo 
+python NCR2utf8.py %1 
+pause 
---
+++ NCR2utf8/NCR2utf8.py
@@ -0,0 +1,12 @@
+from HTMLParser import HTMLParser
+import sys
+filepath = sys.argv[1]
+text = []
+with open(filepath, 'r') as f:
+	lines = f.readlines()
+	for line in lines:
+		text.append(HTMLParser().unescape(line))
+
+with open(str(filepath) + '.utf8.txt', 'w') as w:
+	for line in text:
+		w.write(line.encode('utf-8'))
